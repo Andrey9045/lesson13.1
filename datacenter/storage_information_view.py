3d04268fce19221bbb67572d485e7fd31217dfa1
Andrey9045 <andreidikun123@gmail.com>
2025-04-11 15:56:31 +0400

    Высушил код storage_information_view

--- datacenter/storage_information_view.py
+++ datacenter/storage_information_view.py
@@ -9,22 +9,17 @@ from django.shortcuts import render
 
 def storage_information_view(request):
     # Программируем здесь
-    names = []
-    duration_session = []
-    entry_times = []
     visits = Visit.objects.filter(leaved_at=None)
-    for visit in visits:
-        duration = get_duration(visit)
-        session_duration = format_duration(duration)
-        name = str(visit.passcard)
-        time = str(visit.entered_at)
-        names.append(name)
-        entry_times.append(time)
-        duration_session.append(session_duration)
 
+    non_closed_visits = [
+        {
+            'who_entered': str(visit.passcard),
+            'entered_at': str(visit.entered_at),
+            'duration': format_duration(get_duration(visit))
+        }
+        for visit in visits
+    ]
 
-
-    non_closed_visits = [{"who_entered": a, 'entered_at': b, 'duration': c} for a, b, c in zip(names, entry_times, duration_session)]
     context = {
         'non_closed_visits': non_closed_visits,  # не закрытые посещения
     }
